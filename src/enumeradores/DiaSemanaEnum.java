7ee2693c67ce4f9eab94ebca313254c4dbe552c1
João Lopes <joaomarcoslopes@id.uff.br>
2020-08-17 22:05:07 -0300

    DiaSemanaEnum

---
+++ src/enumeradores/DiaSemanaEnum.java
@@ -0,0 +1,10 @@
+package enumeradores;
+
+/**
+ * DiaSemanaEnum representa os dias da semana usando enum ao invés de constante.
+ * A conveção é usar caixa alta, o mesmo padrão de nomenclatura de constantes
+ */
+
+public enum DiaSemanaEnum {
+  SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+}
